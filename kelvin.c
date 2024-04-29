fbd2609fc967ea80973de3ac936071859a70f4c4
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2024-04-29 13:49:04 +0300

    Update kelvin.c

--- kelvin.c
+++ kelvin.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-       int celsius,son_değer,diğer_sayı;
+       int celsius,son_değer,sayaç;
        float kelvin;
 
        celsius = 0;
-       diğer_sayı = 20;
+       sayaç = 20;
        son_değer = 200;
 
        while(celsius <= son_değer){
             kelvin = celsius + 273.15;
             printf("%d\t%.2f\n",celsius, kelvin);
-            celsius = celsius + diğer_sayı;
+            celsius = celsius + sayaç;
        }
         
         return 0;

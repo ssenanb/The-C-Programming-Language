f92aee873187914e18fca95a7ca5107211d2eab7
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2024-06-03 17:04:22 +0300

    Update aradaki_asallar.c

--- aradaki_asallar.c
+++ aradaki_asallar.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 #include <stdbool.h>
 
-bool asal(int asal){
-    if( asal <= 1) return false;
-    for(int i = 2; i * i <= asal; i++){
-        if(asal % i == 0) return false;
+bool prime(int prime){
+    if( prime <= 1) return false;
+    for(int i = 2; i * i <= prime; i++){
+        if(prime % i == 0) return false;
         }
         return true;
     }
 
 int main()
 {
-    int sayı1,sayı2,i;
-    printf("iki sayı girin: ");
-    scanf("%d %d",&sayı1,&sayı2);
+    int number1,number2,i;
+    printf("enter two numbers: ");
+    scanf("%d %d",&number1,&number2);
 
-    printf("%d ile %d arasındaki asal sayılar\n",sayı1,sayı2);
+    printf("prime numbers between %d and %d\n",sayı1,sayı2);
 
-    for(sayı1; sayı1 <= sayı2; sayı1++){
-        if( asal (sayı1) ){
-            printf("%d ",sayı1);
+    for(number1; number1 <= number2; number1++){
+        if( prime (number1) ){
+            printf("%d ",number1);
         }
     }
 

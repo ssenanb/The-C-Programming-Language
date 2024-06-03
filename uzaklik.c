b8f05df6fca6e17bd5411163983f378c14b016d5
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2024-06-03 17:35:34 +0300

    Update uzaklik.c

--- uzaklik.c
+++ uzaklik.c
@@ -1,27 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    int a,b,c;
-    int x1,y1;
+    int a, b, c;
+    int x1, y1;
     int h1;
     float h2;
 
-    printf("doğru denklemi: ax + by + c = 0\n");
-    printf("doğrunun kat sayılarını sırayla girin: ");
-    scanf("%d %d %d",&a,&b,&c);
+    printf("Line equation: ax + by + c = 0\n");
+    printf("Enter the coefficients of the line in order: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-    printf("nokta: P(x1,y1)\n");
-    printf("uzaklığı hesaplanacak noktanın koordinatlarını girin: ");
-    scanf("%d %d",&x1,&y1);
+    printf("Point: P(x1, y1)\n");
+    printf("Enter the coordinates of the point to calculate the distance: ");
+    scanf("%d %d", &x1, &y1);
 
     h1 = abs((a * x1) + (b * y1) + c);
-    h2 = sqrt(pow(a,2) + pow(b,2));
+    h2 = sqrt(pow(a, 2) + pow(b, 2));
 
-    printf("sonuç: %.2f\n",h1/h2);
+    printf("Result: %.2f\n", h1 / h2);
 
     return 0;
-
 }

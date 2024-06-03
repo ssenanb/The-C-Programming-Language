f9316cc8595d3ef2cb172a425f46b14de06f5844
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2024-06-03 17:10:26 +0300

    Update dizi.c

--- dizi.c
+++ dizi.c
@@ -2,23 +2,20 @@
  
 int main(void)
 {
-     int sayılar[10];
-     int kareler[10];
+     int numbers[10];
+     int squares[10];
      int i,j;
 
      for(i = 1; i <= 10; i++)
      {
-        sayılar[i] = i;
-        kareler[i] = i*i;
+        numbers[i] = i;
+        squares[i] = i*i;
      }
      for(j = 1; j <= 10; j++)
      {
-        printf("%d\n",sayılar[j]);
-        printf("%d\n\n",kareler[j]);
+        printf("%d\n",numbers[j]);
+        printf("%d\n\n",squares[j]);
      }
-
-    
-
     return 0 ;
 
 }

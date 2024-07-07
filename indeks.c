0a26524c4f1305d866d0f6380de570ea587e86ce
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2024-07-07 12:46:35 +0300

    Update indeks.c

--- indeks.c
+++ indeks.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<math.h>
     
-void indeks(double height, int weight)
+float indeks(float height, float weight)
 {
-    int result = (height/(pow(height,2))); 
-    printf("your body mass index:%d\n",result);
+    float result = (height/(pow(height,2))); 
+    printf("your body mass index:%.1f\n",result);
+    return result;
 }
 
 
@@ -12,14 +13,28 @@ int main(void)
 {
     float height;
     int weight;
+    float result;
 
     printf("enter your height in meters ");
     scanf("%f", &height);
 
     printf("enter your weight ");
-    scanf("%d", &weight);
+    scanf("%f", &weight);
 
-    indeks(height,weight);
+    result = indeks(height,weight);
 
+    if(result < 18.5){
+        printf("you are weak\n");
+    }
+    else if(result >= 18.5 && result < 24.9){
+        printf("you are normal\n");
+     }
+     else if(result >= 25 && result < 29.9){
+        printf("you are overweight\n");
+     }
+     else if( result >= 30){
+        printf("you are obesity\n");
+    }
+    
     return 0;
 }
